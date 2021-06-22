b8a94787cf9f588237071b83274d0ca2af94502a
antonio195 <antonio195br@gmail.com>
2021-06-22 00:01:47 -0300

    parei na aula12

---
+++ aula11/js/index.js
@@ -0,0 +1,10 @@
+let num1 = prompt('Digite o primeiro valor');
+let num2 = prompt('Digite o primeiro valor');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+const soma = num1 + num2;
+
+alert(`O resultado da soma dos valores é ${soma}`);
+console.log(`O resultado da soma dos valores é ${soma}`);
---
+++ aula12/index.js
@@ -0,0 +1,12 @@
+let varA = 'A';
+let varB = 'B';
+let varC = 'C';
+let varD = varA;
+
+varA = varB;
+varB = varC;
+varC = varD
+
+console.log(varA)
+console.log(varB)
+console.log(varC)
